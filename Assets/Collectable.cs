7549e0c9c331d729fce11668858c5eef6302ad2f
Umut Muslihoglu <umut.muslihoglu@nosurrender.studio>
2024-10-29 02:20:32 +0300

    Collectable-CollectController-Implemented

---
+++ Assets/Collectable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+   public void Highlight(bool highlight)
+   {
+      // Highlight the collectable
+      
+      if (highlight)
+      {
+         // Highlight the collectable 3d object outline
+         
+      }
+      else
+      {
+         // Remove the highlight from the collectable
+      }
+   }
+}
