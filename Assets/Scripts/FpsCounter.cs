d0bd68385c10db2c7265ff01642a92dac4f07b91
Umut Muslihoglu <umut.muslihoglu@nosurrender.studio>
2024-10-30 13:19:11 +0300

    Update FpsCounter.cs

--- Assets/Scripts/FpsCounter.cs
+++ Assets/Scripts/FpsCounter.cs
@@ -8,13 +8,17 @@ public class FpsCounter : MonoBehaviour
 
     private void Awake()
     {
+#if UNITY_ANDROID || UNITY_IOS 
+        Application.targetFrameRate = 120;
+#endif
+        Application.targetFrameRate = -1; // Set the target frame rate to the maximum
         fpsText = GetComponent<TextMeshProUGUI>();
     }
+
     private void Update()
     {
         _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
-        float msec = _deltaTime * 1000.0f;
-        float fps = 1.0f / _deltaTime;
-        fpsText.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        var fps = 1.0f / _deltaTime;
+        fpsText.text = $"FPS: {Mathf.Ceil(fps)}";
     }
 }
